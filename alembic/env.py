1332d234a851a103f19ab366307643e0f3c9d629
Damien White <damien.white@visoftinc.com>
2018-03-28 13:06:12 -0400

    Added model metadata for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -3,6 +3,15 @@ from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
 
+import os
+import sys
+
+# append current path to get the app
+sys.path.append(os.getcwd())
+
+from shared.models import db
+
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -15,7 +24,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = db.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
