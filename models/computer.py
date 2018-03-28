9d50ca2bbc475bc4de51e710f0d830848e757acd
Damien White <damien.white@visoftinc.com>
2018-03-28 14:11:02 -0400

    Added name to computer model

--- models/computer.py
+++ models/computer.py
@@ -4,5 +4,6 @@ from shared.models import db
 class Computer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     serial = db.Column(db.String)
+    name = db.Column(db.String)
     person_id = db.Column(db.Integer, db.ForeignKey('person.id'))
     person = db.relationship('Person', backref=db.backref('computers'))
